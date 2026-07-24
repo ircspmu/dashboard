448b97c332feb0eb7e6d0b9ef69ac4c5ef547c94
ircspmu <ramosap@irc.pshs.edu.ph>
2026-07-24 16:35:29 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
 
     // After deploying the Apps Script, paste the Web App URL here
     // It will look like: https://script.google.com/macros/s/AKfycbx.../exec
-    APPS_SCRIPT_URL: 'YOUR_APPS_SCRIPT_URL_HERE',
+    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxNz46a09Lz1fyk8GzT_i7T11sE9pzzLJ0OloNXv3W2oeN_dzSv3Zl_tTFBUCUZm2Fk/exec',
 
     // ============================================
     // DISPLAY SETTINGS
